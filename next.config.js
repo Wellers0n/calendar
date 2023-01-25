1f54fea912ce5d10b371f3fdd11a65c4e1b9bd44
Wellerson <wllrsn.c@gmail.com>
2023-01-25 11:40:41 -0300

    fix: modules

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const withTM = require("next-transpile-modules")([
-  "@fullcalendar/common",
+  "@fullcalendar/core",
   "@babel/preset-react",
   "@fullcalendar/common",
   "@fullcalendar/daygrid",
