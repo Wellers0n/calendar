d4ed72b74e58d8582b8a369596a2b781561ab694
Wellerson <wllrsn.c@gmail.com>
2023-01-25 11:30:10 -0300

    feat: import fix error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 // import '../styles/globals.css'
+import '@fullcalendar/react/dist/vdom'
 import "@fullcalendar/common/main.css";
 import "@fullcalendar/daygrid/main.css";
 import "@fullcalendar/timegrid/main.css";
