35948cfe9f4e18cf7a4cbe1217848e64e04db9da
Wellerson <wllrsn.c@gmail.com>
2023-01-25 11:59:48 -0300

    fix: import css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,4 @@
-// import '../styles/globals.css'
 import "@fullcalendar/common/main.css";
-import "@fullcalendar/daygrid/main.css";
-import "@fullcalendar/timegrid/main.css";
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
